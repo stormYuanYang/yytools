14ae84f9096a3d0b4695c5459c94a051cbe46094
yangyuan <yytqmj@vip.qq.com>
2023-06-28 16:08:55 +0800

    更新gcd（求最大公约数）的实现

--- tools/algorithm/math/common.go
+++ tools/algorithm/math/common.go
@@ -22,20 +22,49 @@ import (
 )
 
 /*
- 求最大公约数(欧几里得算法)
+ GcdI-->greatest common divisor recursion
+ 求最大公约数(欧几里得算法)递归实现
  计算两个非负整数x和y的最大公约数:若y是0,则最大公约数为x;
  否则,将x除以y得到余数r,x和y的最大公约数即为y和r的最大公约数.
 
  ⚠️ 注意,这是一个递归函数.
  时间复杂度: ? 直觉一般是常量到对数之间
- 空间复杂度: O(c)
+ 空间复杂度: O(1)
 */
-func Gcd(x, y int) int {
+func GcdR(x, y int) int {
 	assert.Assert(x >= 0, "x must greater than or equal 0, x:", x)
 	assert.Assert(y >= 0, "y must greater than or equal 0, y:", y)
 	if y == 0 {
 		return x
 	}
 	r := x % y
-	return Gcd(y, r)
+	return GcdR(y, r)
+}
+
+/*
+ GcdI-->greatest common divisor iterate
+ 求最大公约数(欧几里得算法)循环遍历实现
+ 计算两个非负整数x和y的最大公约数:若y是0,则最大公约数为x;
+ 否则,将x除以y得到余数r,x和y的最大公约数即为y和r的最大公约数.
+
+ 时间复杂度: ? 直觉一般是常量到对数之间
+ 空间复杂度: O(1)
+*/
+func GcdI(x, y int) int {
+	assert.Assert(x >= 0, "x must greater than or equal 0, x:", x)
+	assert.Assert(y >= 0, "y must greater than or equal 0, y:", y)
+	for {
+		if y == 0 {
+			return x
+		}
+		r := x % y
+		x = y
+		y = r
+	}
+}
+
+func Gcd(x, y int) int {
+	// 遍历(GcdI)比递归(GcdR)效率更高
+	// 这里采用遍历实现的方式获得最大公约数
+	return GcdI(x, y)
 }
